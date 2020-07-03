e7f0ce0f411848d124c90be9245ec3095bacaf47
sidk99 <kaushik.sid.99@berkeley.edu>
2020-07-03 11:37:45 -0700

    planet integration -- still has train input bugs

--- dmbrl/modeling/models/BNN.py
+++ dmbrl/modeling/models/BNN.py
@@ -13,6 +13,58 @@ from dmbrl.modeling.utils import TensorStandardScaler
 from dmbrl.modeling.layers import FC
 from dmbrl.misc.DotmapUtils import *
 
+class VAE():
+    tfd = tf.contrib.distributions
+
+    def make_encoder(data, code_size):
+      x = tf.layers.flatten(data)
+      x = tf.layers.dense(x, 200, tf.nn.relu)
+      x = tf.layers.dense(x, 200, tf.nn.relu)
+      loc = tf.layers.dense(x, code_size)
+      scale = tf.layers.dense(x, code_size, tf.nn.softplus)
+      return tfd.MultivariateNormalDiag(loc, scale)
+
+    def make_prior(code_size):
+      loc = tf.zeros(code_size)
+      scale = tf.ones(code_size)
+      return tfd.MultivariateNormalDiag(loc, scale)
+
+    def make_decoder(code, data_shape):
+      x = code
+      x = tf.layers.dense(x, 200, tf.nn.relu)
+      x = tf.layers.dense(x, 200, tf.nn.relu)
+      logit = tf.layers.dense(x, np.prod(data_shape))
+      logit = tf.reshape(logit, [-1] + data_shape)
+      return tfd.Independent(tfd.Bernoulli(logit), 2)
+
+    make_encoder = tf.make_template('encoder', make_encoder)
+    make_decoder = tf.make_template('decoder', make_decoder)
+
+    data = tf.placeholder(tf.float32, [None, 28, 28])
+
+    prior = make_prior(code_size=2)
+    posterior = make_encoder(data, code_size=2)
+    code = posterior.sample()
+
+    likelihood = make_decoder(code, [28, 28]).log_prob(data)
+    divergence = tfd.kl_divergence(posterior, prior)
+    elbo = tf.reduce_mean(likelihood - divergence)
+
+    optimize = tf.train.AdamOptimizer(0.001).minimize(-elbo)
+    samples = make_decoder(prior.sample(10), [28, 28]).mean()
+
+
+    with tf.train.MonitoredSession() as sess:
+      for epoch in range(20):
+        test_elbo, test_codes, test_samples = sess.run(
+            [elbo, code, samples], {data: test.images})
+        print('Epoch', epoch, 'elbo', test_elbo)
+        plot_codes(test_codes)
+        plot_sample(test_samples)
+        for _ in range(600):
+          sess.run(optimize, {data: train.next_batch(100)[0]})
+
+
 
 class BNN:
     """Neural network models which model aleatoric uncertainty (and possibly epistemic uncertainty
